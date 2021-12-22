24d8480c98f2018d7eb1d6ea8861e1c3307c50b0
mayal22 <amsharma750@gmail.com>
2021-12-22 11:50:42 +0530

    creating files

--- Pageobjects/Base.py
+++ Pageobjects/Base.py
@@ -1,3 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+class basic:
+    @classmethod
+    def createdriver(cls,driver):
+        if driver == "chrome":
+            cls.drivers = webdriver.Chrome(executable_path="D:\\Tasks ERP\\PageObjects\\chromedriver.exe")
+        return cls.drivers
+    def open_tab(self,url):
+        self.driver.execute_script("window.open('');")
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        self.driver.get(url)
+        
+        
+
+        
+
+        
