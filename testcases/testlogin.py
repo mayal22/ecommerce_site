ce1a092117d0ceef5dda9fb5dd3531a0075ee93b
mayal22 <amsharma750@gmail.com>
2021-12-23 10:24:18 +0530

    recover change

--- testcases/testlogin.py
+++ testcases/testlogin.py
@@ -1,8 +1,7 @@
 from Pageobjects.login import Logins
 def test_login():
     ob = Logins()
-    # ob.urlhit()
-    # ob.email()
-    # ob.password()
-    # assert ob.home_pageTitle() == "Men Rocks International | Dashboard","Login was not succesfull"
-    assert True
+    ob.urlhit()
+    ob.email()
+    ob.password()
+    assert ob.home_pageTitle() == "Men Rocks International | Dashboard","Login was not succesfull"
